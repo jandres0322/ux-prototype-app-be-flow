4598af09dedc510eeccb33250284e06f9e780dfa
Liceth Yaneth <liceth.rozo@finkargo.com>
2024-03-13 22:39:02 -0500

    add home screen

---
+++ lib/src/widgets/app_bar_custom_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:ux_prototype_app_be_flow/src/constants/colors.dart';
+import 'package:ux_prototype_app_be_flow/src/providers/user_data_provider.dart';
+import 'package:ux_prototype_app_be_flow/src/widgets/button_custom_widget.dart';
+
+class AppBarCustom {
+  static AppBar buildApp(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    final userDataProvider = Provider.of<UserDataProvider>(context);
+    return AppBar(
+      centerTitle: true,
+      title: ButtonCustom(
+        colorButtonDisabled: AppColors.pomodoro,
+        colorTextDisabled: AppColors.primary,
+        width: size.width * 0.5,
+        height: size.height * 0.05,
+        textButton: "Pomodoro",
+        onPressed: null,
+        isButtonTitle: true,
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {
+            Navigator.pushNamedAndRemoveUntil(context, 'login', (route) => false);
+            userDataProvider.logoutUser();
+          },
+          icon: const Icon(
+            Icons.logout,
+            color: AppColors.primary,
+            size: 30,
+          )
+        )
+      ],
+    );
+  }
+}
